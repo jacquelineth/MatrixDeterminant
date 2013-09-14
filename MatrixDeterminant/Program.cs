6ff58cf9ad29ace5decea4eccf6b38a60119848a
jacquelineth <jacquelineth@gmail.com>
2013-09-14 04:07:52 +0200

    Removed Console output

--- MatrixDeterminant/Program.cs
+++ MatrixDeterminant/Program.cs
@@ -24,21 +24,12 @@ namespace MatrixDeterminant
 			return getNextIndex (previous, size, true);
 		}
 
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
-			MainClass c = new MainClass ();
-			int[] a = {1,2,3,4,5,6,7,8,9  };
-			//int[] a = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-			c.Puzzle(a);
-			//Console.ReadKey ();
 
-		}
 		int Puzzle( int [] a){
 			int length = a.Length;
 			int size = (int) Math.Sqrt(a.Length);
 			int pas = size + 1;
-			int rlsum = 1;int lrsum = 1;int lastIndex = 0;
+			int rlsum = 1;int lrsum = 1;
 			for (int i =0; i<size; i++) {
 				int product = 1;
 				 
@@ -49,25 +40,11 @@ namespace MatrixDeterminant
 					else 
 						index = i + (j-1)*pas+ 1 ;
 					product *= a[index];
-					Console.Write ("\t {0}",index);
 				}
 				rlsum += product;
-				Console.WriteLine ("\t Product :{0}",product);
+
 			}
-			//return rlsum;
-			Console.WriteLine ("\n");
-		/*	for (int i =size-1; i >=0; --i) {
-				int product = 1;
-				for (int j=0; j <size; j++) {
-					int index = i  + j * size-j;
-					if (index < j * size || index > (j + 1) * size)
-						index = i + j * (size+ 1)-1;
-					product *= a [index];
-					Console.Write ("\t {0}",index);
-				}
-				lrsum += product;
-				Console.WriteLine ("\t Product :{0}",product);
-			}*/
+
 
 			int[,] matrix =  new int [size,size];
 			int mindex =0;
@@ -80,13 +57,10 @@ namespace MatrixDeterminant
 				for (int j=0; j <size; j++) {//Line
 					int xN = getNextDecIndex (x, size);
 					product *= matrix[j,xN];
-					Console.Write ("a[{0},{1}] x{2}   ",j,xN,x);
 					x=xN;
 				}
 				lrsum += product;
-				Console.WriteLine ("\t Product :{0}",product);
 			}
-			Console.WriteLine ("\t Result :{0}",rlsum-lrsum);
 			return rlsum-lrsum;
 
 		} 

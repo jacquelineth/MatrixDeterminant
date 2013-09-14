1c8c90f67715845bb890da26f4900c3b27095889
jacquelineth <jacquelineth@gmail.com>
2013-09-14 09:56:01 +0200

    Version for pex
    Added NUnit test

--- MatrixDeterminant/NUnitTestClass.cs
+++ MatrixDeterminant/NUnitTestClass.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 namespace MatrixDeterminant
 {
 	[TestFixture()]
-	public class MatrixDeterminantTest
+	public class ProgramTest
 	{
 		[Test()]
 		public void getNextIncIndex ()
@@ -12,7 +12,7 @@ namespace MatrixDeterminant
 			int[] a = {1,2,0};
 			int[] b = {2,0,1,2,1};
 			for (int i = 0; i < a.Length; i++) {
-				int result=MatrixDeterminant.MainClass.getNextIncIndex(a[i],3) ;
+				int result=Program.getNextIncIndex(a[i],3) ;
 				Assert.AreEqual (b[i],result,"input "+a[i]+" result:"+result);
 			}
 
@@ -23,13 +23,25 @@ namespace MatrixDeterminant
 			int[] a = {1,2,0 };
 			int[] b = {0,1,2,1,1};
 			for (int i = 0; i < a.Length; i++) {
-				int result=MatrixDeterminant.MainClass.getNextDecIndex(a[i],3) ;
+				int result=Program.getNextDecIndex(a[i],3) ;
 
 				Assert.AreEqual (b[i], result,"input "+a[i]+" result:"+result);
 			}
 
 		}
-
+		[Test()]
+		public void Puzzle2x2(){
+			int[] a = { 1, 2, 3, 4 };
+			int result = Program.Puzzle (a);
+			Assert.AreEqual (0,result);
+		}
+		[Test()]
+		public void Puzzle3x3(){
+			int[] a = { 1,2,3,4,5,6,7,8,9   };
+			int result = Program.Puzzle (a);
+			int correct= a[0]*a[4]*a[8]+a[1]*a[5]*a[6]+a[2]*a[3]*a[7] -(a[2]*a[4]*a[6]+a[1]*a[3]*a[8]+a[0]*a[7]*a[5]);
+			Assert.AreEqual (correct,result);
+		}
 	}
 }
 
--- MatrixDeterminant/Program.cs
+++ MatrixDeterminant/Program.cs
@@ -2,9 +2,47 @@ using System;
 
 namespace MatrixDeterminant
 {
-	class MainClass
+	class Program
 	{
-		 static int getNextIndex(int previous, int size, bool increase){
+		public static int Puzzle( int [] a){
+			int length = a.Length;
+			int size = (int) Math.Sqrt(a.Length);
+			int pas = size + 1;
+			int rlsum = 1;int lrsum = 1;
+
+			for (int i =0; i<size; i++) {
+				int product = 1;
+				for (int j=0; j<size; j++) {
+					int index = 0;
+					if (i + j*pas < (j+1)*size)
+						index = i + j*pas;
+					else 
+						index = i + (j-1)*pas+ 1 ;
+					product *= a[index];
+				}
+				rlsum += product;
+			}
+
+			int[,] matrix =  new int [size,size];
+			int mindex =0;
+			foreach (var item in a) {
+				matrix[mindex/size,mindex%size]=a[mindex];
+				mindex++;
+			}
+			for (int i = size; i > 0; --i) {//columnm
+				int product = 1;int x = i;
+				for (int j=0; j <size; j++) {//Line
+					int xN = getNextDecIndex (x, size);
+					product *= matrix[j,xN];
+					x=xN;
+				}
+				lrsum += product;
+			}
+			return rlsum-lrsum;
+		} 
+
+
+		static int getNextIndex(int previous, int size, bool increase){
 			size--;//to match index
 		previous=(increase)?previous+1:previous-1;
 			if (!increase) {
@@ -23,46 +61,16 @@ namespace MatrixDeterminant
 		 public static int getNextIncIndex(int previous, int size){
 			return getNextIndex (previous, size, true);
 		}
+		public static void Main (string[] args)
+		{
+			
+			Program c = new Program ();
+			int[] a = {1,2,3,4,5,6,7,8,9  };
+			//int[] a = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+			int res= Program.Puzzle(a);
+			Console.WriteLine ("Hello World!, {0}",res);
+			//Console.ReadKey ();
 
-
-		int Puzzle( int [] a){
-			int length = a.Length;
-			int size = (int) Math.Sqrt(a.Length);
-			int pas = size + 1;
-			int rlsum = 1;int lrsum = 1;
-			for (int i =0; i<size; i++) {
-				int product = 1;
-				 
-				for (int j=0; j<size; j++) {
-					int index = 0;
-					if (i + j*pas < (j+1)*size)
-						index = i + j*pas;
-					else 
-						index = i + (j-1)*pas+ 1 ;
-					product *= a[index];
-				}
-				rlsum += product;
-
-			}
-
-
-			int[,] matrix =  new int [size,size];
-			int mindex =0;
-			foreach (var item in a) {
-				matrix[mindex/size,mindex%size]=a[mindex];
-				mindex++;
-			}
-			for (int i = size; i > 0; --i) {//columnm
-				int product = 1;int x = i;
-				for (int j=0; j <size; j++) {//Line
-					int xN = getNextDecIndex (x, size);
-					product *= matrix[j,xN];
-					x=xN;
-				}
-				lrsum += product;
-			}
-			return rlsum-lrsum;
-
-		} 
+		}
 	}
 }
